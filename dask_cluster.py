45695ca7a4bc5057bdad29075f2883c419ce3e9a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2025-04-02 22:06:14 +0100

    Re-organise code

--- dask_cluster.py
+++ dask_cluster.py
@@ -25,18 +25,14 @@ def run_analysis(client, image_data):
         futures.append(client.submit(analyze, image_data, x))
     return futures
 
-def main():
-    cluster = LocalCluster()
-    # read the image as dask array
-    # Code tests on a 5D-image
+if __name__ == "__main__":
     path_to_image = "/path_to_image"
     img = BioImage(path_to_image)
     print(img.dask_data.shape)
-    with Client(cluster) as client: # This ensure that we close at the end
-        # perform code
-        futures = run_analysis(client, img.dask_data)
-        results = client.gather(futures)
-    print(results)
+    futures = []
+    values = [50, 100, 150]
 
-if __name__ == "__main__":
-    main()
+    client = Client(processes=False, threads_per_worker=1)
+    futures = run_analysis(client, img.dask_data)
+    results = client.gather(futures)
+    print(results)
